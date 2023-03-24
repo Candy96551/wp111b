a3473d37a4c19aef6e30deb45defa56e0b9c25ee
candy96551 <sean0206w@gmail.com>
2023-03-24 11:36:27 +0800

    IsPrime

--- js/truthtable.js
+++ js/truthtable.js
@@ -1,7 +1,7 @@
 function truthTable(n)
 {
-   var t= Math.pow(2,n)
-   for(var i=0; i<t; i++)
+   var a= Math.pow(2,n)
+   for(var i=0; i<a; i++)
    {
     var x= i.toString(2).padStart(n,'0')
     console.log(x)
